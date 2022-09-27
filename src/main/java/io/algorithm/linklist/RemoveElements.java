d22649cd6260c0b5e41780fe6cd6da9c16999f49
maoling.ml <maoling.ml@antgroup.com>
2022-09-27 20:00:45 +0800

    feat: leetcode 203 移除链表元素

---
+++ src/main/java/io/algorithm/linklist/RemoveElements.java
@@ -0,0 +1,49 @@
+/*
+ * Ant Group
+ * Copyright (c) 2004-2022 All Rights Reserved.
+ */
+package io.algorithm.linklist;
+
+/**
+ *
+ * leetcode 203 移除链表元素
+ *
+ *
+ * @author maoling
+ * @version RemoveElements.java, v 0.1 2022年09月27日 19:49 maoling
+ */
+public class RemoveElements {
+
+    public static Node removeElements(Node head,int val){
+        if(head == null) return null;
+        head.next = removeElements(head.next,val);
+        return head.val == val ? head.next : head;
+    }
+
+    public static void main(String[] args) {
+        Node node01 = new Node();
+        node01.val = 1;
+        Node node02 = new Node();
+        node02.val = 2;
+        Node node03 = new Node();
+        node03.val = 3;
+        Node node04 = new Node();
+        node04.val = 4;
+        node01.next = node02;
+        node02.next = node03;
+        node03.next = node04;
+
+        Node node = removeElements(node01, 2);
+
+        while (node != null) {
+            System.out.println(node.val);
+            node = node.next;
+        }
+
+    }
+
+    static class Node{
+        int val;
+        Node next;
+    }
+}
